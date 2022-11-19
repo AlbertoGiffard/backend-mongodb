fc09cc97aea27e08783c81ac1b23bd1a6946bec7
AlbertoGiffard <albertoggm1@gmail.com>
2022-11-19 11:54:53 -0300

    updates

---
+++ functions/index.js
@@ -0,0 +1,25 @@
+const functions = require("firebase-functions");
+const express = require('express');
+const connectDb = require('../config/');
+const cors = require('cors');
+const { response } = require("express");
+
+const app = express();
+
+//conectamos a la BD
+connectDb();
+app.use(cors());
+
+app.use(express.json());
+
+app.use('/', require('./routes/tickets'));
+
+app.get('/timestamp', (request, response) => {
+    response.send(`${Date.now()}`);
+})
+
+exports.app = functions.https.onRequest(app);
+
+/* app.listen(4000, () => {
+    console.log("funciona todo bien");
+}) */
